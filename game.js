4da80e5cd98f2a82c67d6f2e8909a2a6806e0dc4
dussy42 <98059288+dussy42@users.noreply.github.com>
2023-06-08 21:39:22 +0100

    The Rules  for the game   The javacript for  the rules button was created( onclick of the   rules button will show  a drop down of the rules for the quiz).Hence   clicking the rules button again will go back to default.

--- game.js
+++ game.js
@@ -1,14 +1,30 @@
 function rule(){
-    var ul=document.getElementsByTagName("ul");
-    /*
-    document.getElementsByTagName("ul").setAttribute("class","tas");
-    ul.setAttribute("class","tabs"); */
+    var btn=document.getElementById("toggleBtn");
+
     
-    var listItems=document.getElementsByTagName("p")
+    if (btn.value=="ON"){
+    var ul=document.getElementsByTagName("ul");
+    var listItems=document.getElementsByTagName("p");
+   
     ul.innerText="Game Rules";
     listItems[0].innerText="Click the start game button";
     listItems[1].innerText="A score will be rendered onscreen";
     listItems[2].innerText="Press Hold button to push your score to the score button";
     listItems[3].innerText="While on hold, the second Player could do the same as in 1 and two";
     
+    btn.value="OFF"
  }
+ else{
+    var ul=document.getElementsByTagName("ul");
+    var listItems=document.getElementsByTagName("p")
+    ul.innerText="Game Rules";
+    listItems[0].innerText="";
+    listItems[1].innerText="";
+    listItems[2].innerText="";
+    listItems[3].innerText="";
+    btn.value="ON"
+    
+ }
+};
+
+ 
