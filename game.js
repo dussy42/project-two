38ff1550ac5e2b056ccc1487a4e28222140fe5cd
dussy42 <98059288+dussy42@users.noreply.github.com>
2023-06-09 12:36:33 +0100

    Updated Asset folder,html, js and css file The comptia img was added to the asset folder and question 2 and three were added to the html file. Therefore, the style for the former and later were added to the css file. In addition, the js code was written to prevent the user from clicking multiple options and whenever an option is clicked, the right answer will be shown in the option

--- game.js
+++ game.js
@@ -27,4 +27,139 @@ function rule(){
  }
 };
 
- 
+/* getting username from input field */
+
+function inpt(){
+    var inputEl=document.getElementById("myname");
+    var defaultVal=inputEl.defaultValue;
+     var currentVal=inputEl.value;
+
+   if (defaultVal==currentVal){
+   document.getElementById("nameEl").innerHTML=" Continue as a guest";
+}
+else{
+    document.getElementById("nameEl").innerHTML= "Welcome " + currentVal;
+    document.getElementById("nameEl").style.color="white";
+    document.getElementById("nameEl").className="names"
+
+}
+};
+
+
+
+/* the definition of correct answer for question 1*/
+let boxes=document.getElementsByTagName("li");
+for(let box of boxes){
+    box.addEventListener("click", function onClick(){
+        var rightAns=document.getElementById("fourth")
+        let correct=rightAns;
+
+        //disable other options
+            for(let option of boxes){
+                option.style.pointerEvents="none"
+            }
+            if(correct===firstQues){
+                first()
+            }
+            else if(correct===secondQues){
+                sec()
+            }
+            else if(correct===thirdQues){
+                third()
+            }
+            else{
+                rightAns.innerHTML= "EIDE port" + " correct answer";
+            rightAns.style.backgroundColor="white";
+            }
+    });
+}
+
+/* declaring option one if clicked */
+let firstQues=document.getElementById("first");
+firstQues.addEventListener("click", function first(){
+firstQues.style.backgroundColor="goldenrod"
+
+});
+
+/* declaring option two if clicked */
+let secondQues=document.getElementById("second");
+secondQues.addEventListener("click", function sec(){
+secondQues.style.backgroundColor="goldenrod"
+
+});
+
+/* declaring option third if clicked */
+let thirdQues=document.getElementById("third");
+thirdQues.addEventListener("click", function third(){
+thirdQues.style.backgroundColor="goldenrod";
+
+});
+
+
+
+
+/* function to reveal answer*/
+function rev(){
+  document.getElementById("pan").textContent= "EIDE port";
+   document.getElementById("pan").style.backgroundColor="white"
+};
+
+
+
+/* javacript codes for the second question */
+
+var oils=document.getElementsByClassName("opt");
+for(let oil of oils){
+    oil.addEventListener("click", function palm(){
+    var bestAns=document.getElementById("opt2");
+    let correctAns=bestAns;
+
+    //disable other options
+    for(let option of oils){
+        option.style.pointerEvents="none";
+    }
+    if(correctAns===quesTwo1){
+       ir()
+    }
+    
+   else if(correctAns===quesTwo3){
+        lte()
+    }
+
+  else if(correctAns===quesTwo4){
+          blueTooth()
+   }
+
+   else{
+    bestAns.innerHTML="NFC" + " correct answer";
+    bestAns.style.backgroundColor="white";
+   }
+        
+    });
+
+}
+
+/* declaring option one in question 2 if clicked */
+var quesTwo1=document.getElementById("opt1");
+quesTwo1.addEventListener("click", function ir(){
+    quesTwo1.style.backgroundColor="goldenrod"
+})
+
+
+ /* declaring option 3 in question 2 if clicked */
+ var quesTwo3=document.getElementById("opt3");
+ quesTwo3.addEventListener("click", function lte(){
+    quesTwo3.style.backgroundColor="goldenrod"
+ })
+
+ /* declaring option 4 in question 2 if clicked */
+ var quesTwo4=document.getElementById("opt4");
+ quesTwo4.addEventListener("click", function blueTooth(){
+    quesTwo4.style.backgroundColor="goldenrod"
+ });
+
+/*revealing the answers for question2*/
+function showAns(){
+    document.getElementById("wireless").textContent="NFC";
+    document.getElementById("wireless").style.backgroundColor="white";
+};
